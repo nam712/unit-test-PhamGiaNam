f727b6c39af98bbe0b2e76bf60daa4257cda7c61
nam712 <namchetql169@gmail.com>
2025-05-21 15:52:31 +0700

    student

---
+++ src/main/java/com/mycompany/student/analyzer/StudentAnalyzer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student.analyzer;
+
+/**
+ *
+ * @author PC
+ */
+public class StudentAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
